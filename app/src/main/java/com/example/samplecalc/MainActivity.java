3c91f09fb3fc686058ba227b236f6b8b4eaa5c73
oddballs1111 <77184451+oddballs1111@users.noreply.github.com>
2021-01-24 12:50:30 +0900

    電卓アプリ　第二回 　・Selectorの追加 　・Relative/Frame Layoutのサンプル追加（簡易的なもの） 　・ConstraintLayoutのサンプル追加（簡易的なもの） 　・横画面のレイアウト追加 　・PlantUMLの追加（デフォルト状態のもの）

--- app/src/main/java/com/example/samplecalc/MainActivity.java
+++ app/src/main/java/com/example/samplecalc/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_calc);
+        setContentView(R.layout.constraint_calc);
+
     }
 }
